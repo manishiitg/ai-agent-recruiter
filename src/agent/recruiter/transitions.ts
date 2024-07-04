8a7a6d63ad973ff58691f3c339f61f2a6875320e
Manish Prakash <manish.prakash@citymall.live>
2024-07-04 13:08:00 +0530

    setup new relic

--- src/agent/recruiter/transitions.ts
+++ src/agent/recruiter/transitions.ts
@@ -1,16 +1,18 @@
-import { STAGE_GOT_CTC, STAGE_GOT_RESUME, STAGE_NEW } from "./agent";
+import { STAGE_GOT_CTC, STAGE_GOT_REJECTED, STAGE_GOT_RESUME, STAGE_NEW } from "./agent";
 import { Conversation } from "./types/conversation";
 
 export const transitionStage = async (conversation: Conversation) => {
-  if (conversation?.resume?.full_resume_text) {
-    if (conversation?.stage == STAGE_NEW && conversation.resume?.full_resume_text?.length > 0) {
-      return STAGE_GOT_RESUME;
+  if (conversation.stage !== STAGE_GOT_REJECTED) {
+    if (conversation?.resume?.full_resume_text) {
+      if (conversation?.stage == STAGE_NEW && conversation.resume?.full_resume_text?.length > 0) {
+        return STAGE_GOT_RESUME;
+      }
     }
-  }
-  const info = conversation?.info;
-  if (info?.expected_ctc && info.expected_ctc != "no") {
-    if (conversation && conversation.resume?.full_resume_text && conversation) {
-      return STAGE_GOT_CTC;
+    const info = conversation?.info;
+    if (info?.expected_ctc && info.expected_ctc != "no") {
+      if (conversation && conversation.resume?.full_resume_text && conversation) {
+        return STAGE_GOT_CTC;
+      }
     }
   }
   return "";
