3b1b3ba809070f4b59d99d8d2227428ff716ba58
Manish Prakash <manish.prakash@citymall.live>
2024-07-11 13:19:32 +0530

    updates

--- src/agent/recruiter/rule_map.ts
+++ src/agent/recruiter/rule_map.ts
@@ -154,10 +154,7 @@ export const STAGE_RULE_MAP: Record<
     },
     do_call_via_human: {
       rule: "if candidate has provided his phone number and also provided his availability",
-      response: `mention that you are shortlisted and we will start next round soon.
-      Inform candidate they can reach our HR team at +91-9580421704 (Anishka) for job profile Python.
-      Or +91-8534091412(Sanjana) for Reactjs, PHP, Vuejs
-      Check which job profile the candidate belongs to before share the phone no`,
+      response: `mention that you are shortlisted and we will start next round soon.`,
       // response: "mention i will call you later on and mention his phone no",
     },
     tell_job_description: {
