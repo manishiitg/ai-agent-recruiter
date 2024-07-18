546634ff127d5c12b79323c44b2cb748c3f5b484
Manish Prakash <manish.prakash@citymall.live>
2024-07-18 08:55:49 +0530

    updates

--- src/agent/jobconfig.ts
+++ src/agent/jobconfig.ts
@@ -138,7 +138,9 @@ export const linkedJobProfileRules: Record<
       `how do you perform raw SQL queries in Django`,
       `assume there is an array on integers and we need to find the nth largest element in the array. what would be your approach to find this. don't use sorting, need to use for loops and solve this problem in O(n) time complexity. let me know what be your approach`,
       `what middleware is in Django`,
-      `what is left join in mysql`,
+      `left join vs inner join in mysql`,
+      `techical question related to python`,
+      `techical question related to python`
     ],
   },
   "Python Internship (Cuvette)": {
