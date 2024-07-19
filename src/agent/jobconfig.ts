02c2eef176916c09b1f8d2d1427ac6e394b270af
Manish Prakash <manish.prakash@citymall.live>
2024-07-19 14:51:26 +0530

    updates

--- src/agent/jobconfig.ts
+++ src/agent/jobconfig.ts
@@ -87,7 +87,7 @@ export const linkedJobProfileRules: Record<
     questions_to_ask: [`candidate introduction about yourself and your projects`, `what is states vs props`, `question about state managment`, `vuejs interview questions`],
   },
   "Jr NodeJs Developer": {
-    is_open: true,
+    is_open: false,
     job_description: `Good Knowledge in expressjs or typescript or other nodejs frameworks.
     Worked with databases like mongodb or mysql or postgresql`,
     // full_criteria: `
