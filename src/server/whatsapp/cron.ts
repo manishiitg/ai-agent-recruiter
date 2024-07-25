e09c69a0966fdba23fda9123c52a43e9e7c462ad
Manish Prakash <manish.prakash@citymall.live>
2024-07-25 19:48:16 +0530

    updates

--- src/server/whatsapp/cron.ts
+++ src/server/whatsapp/cron.ts
@@ -315,7 +315,7 @@ export const archieve_candidate = async () => {
 
 export const start_cron = async () => {
   await archieve_candidate()
-  await evaluate_hr_screen_interview();
+  // await evaluate_hr_screen_interview();
   // check_slack_thread_for_manual_msgs();
   // await get_pending_hr_screening_candidates();
 
@@ -326,8 +326,8 @@ export const start_cron = async () => {
     //send remainders to candidate on same day
     // await check_slack_thread_for_manual_msgs();
     // await keep_conversation_warm();
-    await evaluate_hr_screen_interview();
-    await archieve_candidate();
+    // await evaluate_hr_screen_interview();
+    // await archieve_candidate();
   }, 1000 * 60 * 30); //30min
 
   setInterval(() => {
