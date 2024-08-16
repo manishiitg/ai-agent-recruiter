8911a1a360cda56cba0e7d0a8ece54f90d66d2ec
Manish Prakash <manish.prakash@citymall.live>
2024-08-16 09:28:00 +0530

    updates

--- src/llms/index.ts
+++ src/llms/index.ts
@@ -1,3 +1,4 @@
+import { CLAUDE_HAIKU, CLAUDE_SONNET } from "./claude";
 import { callDeepkSeekLLM, callDeepseekMessages, DEEP_SEEK_V2_CHAT, DEEP_SEEK_V2_CODER } from "./deepkseek";
 
 export async function callViaMessages(
@@ -21,6 +22,8 @@ export async function callViaMessages(
 
 export async function callLLM(prompt: string, user: string, temperature = 0, model = DEEP_SEEK_V2_CODER, meta = {}, cb: (output: string) => Promise<Record<string, string>>) {
   if (model === DEEP_SEEK_V2_CODER || model === DEEP_SEEK_V2_CHAT) {
+    return callDeepkSeekLLM(prompt, user, temperature, model, meta, cb);
+  } else if (model == CLAUDE_HAIKU || model == CLAUDE_SONNET) {
     return callDeepkSeekLLM(prompt, user, temperature, model, meta, cb);
   }
   throw new Error(`${model} not found!`);
