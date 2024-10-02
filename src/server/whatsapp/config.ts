6d7be6e434a38a4a09cbb51e928b894ae7d2e674
Manish Prakash <manish.prakash@citymall.live>
2024-10-02 12:48:58 +0530

    updates

--- src/server/whatsapp/config.ts
+++ src/server/whatsapp/config.ts
@@ -1,3 +1,3 @@
 export const CLOSE_BOT = false;
-export const CLOSE_INTERVIEW = false;
+export const CLOSE_INTERVIEW = true;
 export const ALLOW_SPECIFIC_USERS: Record<string, boolean> = { "919717071555": true };
